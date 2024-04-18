74ec2aab40d5ffd2e030c52f5ea8d74274deee91
Macintosh <macintosh@Macintoshs-MacBook-Air.local>
2024-04-18 21:29:43 +0700

    fix nav

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,49 +1,51 @@
 <template>
-  <nav
-    class="bg-gray-900 text-gray-100 py-3.5 px-6 shadow md:flex justify-between item-center"
-  >
-    <div class="flex items-center cursor-pointer">
-      <span class="text-green-500 text-xl mr-1">
-        <router-link to="/" exact>
-          <div class="logo-container">
-            <img src="../assets/logo.png" alt="" />
-          </div>
-        </router-link>
-      </span>
-    </div>
-    <!-- Menu toggle button -->
-    <span
-      @click="MenuOpen()"
-      class="absolute right-6 top-1.5 cursor-pointer md:hidden text-4xl"
+  <div class="bg-gray-900 flex justify-center items-center">
+    <nav
+      class="container text-gray-100 py-3.5 px-6 justify-between shadow md:flex"
     >
-      <i :class="[open ? 'bi bi-x' : 'bi bi-filter-left']"></i>
-    </span>
-    <!-- Navigation menu -->
-    <ul
-      :class="[open ? 'left-0' : 'left-full hidden']"
-      class="md:flex md:items-center md:px-0 px-10 md:pb-0 pb-10 md:static absolute bg-gray-900 md:w-auto w-full top-14 duration-300 ease-in left-0"
-    >
-      <li
-        class="md:mx-4 md:my-0 my-6 text-left"
-        v-for="link in Links"
-        :key="link.name"
+      <div class="flex items-center cursor-pointer">
+        <span class="text-green-500 text-xl mr-1">
+          <router-link to="/" exact>
+            <div class="logo-container">
+              <img src="../assets/logo.png" alt="" />
+            </div>
+          </router-link>
+        </span>
+      </div>
+      <!-- Menu toggle button -->
+      <span
+        @click="MenuOpen()"
+        class="absolute right-6 top-1.5 cursor-pointer md:hidden text-4xl"
       >
-        <router-link
-          :to="link.link"
-          class="text-xl text-white hover:text-green-500"
-          :exact-active-class="'router-link-exact-active'"
+        <i :class="[open ? 'bi bi-x' : 'bi bi-filter-left']"></i>
+      </span>
+      <!-- Navigation menu -->
+      <ul
+        :class="[open ? 'left-0' : 'left-full hidden']"
+        class="md:flex md:items-center md:px-0 px-10 md:pb-0 pb-10 md:static absolute bg-gray-900 md:w-auto w-full top-14 duration-300 ease-in left-0"
+      >
+        <li
+          class="md:mx-4 md:my-0 my-6 text-left"
+          v-for="link in Links"
+          :key="link.name"
         >
-          {{ link.name }}
-        </router-link>
-      </li>
-      <li class="md:mx-4 md:my-0 my-6 text-left">
-        <Button class="md:w-auto w-full" :goTo="Login">Sign In</Button>
-      </li>
-      <li class="md:mx-4 md:my-0 my-6 text-left">
-        <Button class="md:w-auto w-full" :goTo="Register">Register</Button>
-      </li>
-    </ul>
-  </nav>
+          <router-link
+            :to="link.link"
+            class="text-xl text-white hover:text-green-500"
+            :exact-active-class="'router-link-exact-active'"
+          >
+            {{ link.name }}
+          </router-link>
+        </li>
+        <li class="md:mx-4 md:my-0 my-6 text-left">
+          <Button class="md:w-auto w-full" :goTo="Login">Sign In</Button>
+        </li>
+        <li class="md:mx-4 md:my-0 my-6 text-left">
+          <Button class="md:w-auto w-full" :goTo="Register">Register</Button>
+        </li>
+      </ul>
+    </nav>
+  </div>
 </template>
 
 <script>
